6800512c789642cb00333e88cc6038a377fd2b22
Alex Hong <thealexhong@gmail.com>
2015-12-18 20:48:26 -0500

    Skeleton variables

---
+++ starship/NuiSkeletonPointsViewer.cpp
@@ -0,0 +1,159 @@
+/**
+* NuiSkeletonPointsViewer.cpp
+* alex.hong@mail.utoronto.ca
+*/
+
+#include "stdafx.h"
+#include <limits>
+#include "NuiSkeletonPointsViewer.h"
+#include "resource.h"
+
+/**
+ * Constructor
+ * @param  pParent  The pointer to parent window
+ */
+NuiSkeletonPointsViewer::NuiSkeletonPointsViewer(const NuiViewer* pParent) : NuiViewer(pParent),
+                                                                           m_headX(FLT_MAX),       m_headY(FLT_MAX),       m_headZ(FLT_MAX),
+																		   m_cshouldersX(FLT_MAX), m_cshouldersY(FLT_MAX), m_cshouldersZ(FLT_MAX),
+																		   m_spineX(FLT_MAX),      m_spineY(FLT_MAX),      m_spineZ(FLT_MAX),
+																		   m_chipX(FLT_MAX),       m_chipY(FLT_MAX),       m_chipZ(FLT_MAX),
+																		   m_rhandX(FLT_MAX),      m_rhandY(FLT_MAX),      m_rhandZ(FLT_MAX),
+																		   m_rwristX(FLT_MAX),     m_rwristY(FLT_MAX),     m_rwristZ(FLT_MAX),
+																		   m_relbowX(FLT_MAX),     m_relbowY(FLT_MAX),     m_relbowZ(FLT_MAX),
+																		   m_rshoulderX(FLT_MAX),  m_rshoulderY(FLT_MAX),  m_rshoulderZ(FLT_MAX),
+																		   m_rhipX(FLT_MAX),       m_rhipY(FLT_MAX),       m_rhipZ(FLT_MAX),
+																		   m_rkneeX(FLT_MAX),      m_rkneeY(FLT_MAX),      m_rkneeZ(FLT_MAX),
+																		   m_rankleX(FLT_MAX),     m_rankleY(FLT_MAX),     m_rankleZ(FLT_MAX),
+																		   m_rfootX(FLT_MAX),      m_rfootY(FLT_MAX),      m_rfootZ(FLT_MAX),
+																		   m_lhandX(FLT_MAX),      m_lhandY(FLT_MAX),      m_lhandZ(FLT_MAX),
+																		   m_lwristX(FLT_MAX),     m_lwristY(FLT_MAX),     m_lwristZ(FLT_MAX),
+																		   m_lelbowX(FLT_MAX),     m_lelbowY(FLT_MAX),     m_lelbowZ(FLT_MAX),
+																		   m_lshoulderX(FLT_MAX),  m_lshoulderY(FLT_MAX),  m_lshoulderZ(FLT_MAX),
+																		   m_lhipX(FLT_MAX),       m_lhipY(FLT_MAX),       m_lhipZ(FLT_MAX),
+																		   m_lkneeX(FLT_MAX),      m_lkneeY(FLT_MAX),      m_lkneeZ(FLT_MAX),
+																		   m_lankleX(FLT_MAX),     m_lankleY(FLT_MAX),     m_lankleZ(FLT_MAX),
+																		   m_lfootX(FLT_MAX),      m_lfootY(FLT_MAX),      m_lfootZ(FLT_MAX)
+
+{
+}
+
+
+/** 
+ * Destructor
+ */
+NuiSkeletonPointsViewer::~NuiSkeletonPointsViewer()
+{
+}
+
+/**
+* Process message or send it to coreponding handler
+* @param   hWnd    The handle to the window which receives the message
+* @param   uMsg    Message identifier
+* @param   wParam  Additional message information
+* @param   lParam  Additional message information
+* @return  Result of message process. Depends on message type
+*/
+LRESULT NuiSkeletonPointsViewer::DialogProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	// Pass the message to default dialog procedure
+	return FALSE;
+}
+
+/**
+ * Returns the id of the dialog
+ */
+UINT NuiSkeletonPointsViewer::GetDlgId()
+{
+	return IDD_SKELETON_VIEW;
+}
+
+/**
+* Set skeleton 3D position readings to display
+* @param  headx
+* ...
+* @param lfoot
+*/
+void NuiSkeletonPointsViewer::SetSkeletonPointsReadings(FLOAT headx, FLOAT heady, FLOAT headz,
+                                                        FLOAT cshoulderx, FLOAT cshouldery, FLOAT cshoulderz,
+														FLOAT spinex, FLOAT spiney, FLOAT spinez,
+														FLOAT chipx, FLOAT chipy, FLOAT chipz,
+														FLOAT rhandx, FLOAT rhandy, FLOAT rhandz,
+														FLOAT rwristx, FLOAT rwristy, FLOAT rwristz,
+														FLOAT relbowx, FLOAT relbowy, FLOAT relbowz,
+														FLOAT rshoulderx, FLOAT rshouldery, FLOAT rshoulderz,
+														FLOAT rhipx, FLOAT rhipy, FLOAT rhipz,
+														FLOAT rkneex, FLOAT rkneey, FLOAT rkneez,
+														FLOAT ranklex, FLOAT rankley, FLOAT ranklez,
+														FLOAT rfootx, FLOAT rfooty, FLOAT rfootz,
+														FLOAT lhandx, FLOAT lhandy, FLOAT lhandz,
+														FLOAT lwristx, FLOAT lwristy, FLOAT lwristz,
+														FLOAT lelbowx, FLOAT lelbowy, FLOAT lelbowz,
+														FLOAT lshoulderx, FLOAT lshouldery, FLOAT lshoulderz,
+														FLOAT lhipx, FLOAT lhipy, FLOAT lhipz,
+														FLOAT lkneex, FLOAT lkneey, FLOAT lkneez,
+														FLOAT lanklex, FLOAT lankley, FLOAT lanklez,
+														FLOAT lfootx, FLOAT lfooty, FLOAT lfootz)
+{
+	// Format the readings and update to static control
+	// Fuck this doesn't scale... whatever
+	CompareUpdateValue(headx, m_headX, m_hWnd, IDC_SKELETON_HEAD_X_READING, L"%.2fg");
+	CompareUpdateValue(heady, m_headY, m_hWnd, IDC_SKELETON_HEAD_Y_READING, L"%.2fg");
+	CompareUpdateValue(headz, m_headZ, m_hWnd, IDC_SKELETON_HEAD_Z_READING, L"%.2fg");
+	CompareUpdateValue(cshoulderx, m_cshouldersX, m_hWnd, IDC_SKELETON_CSHOULDER_X_READING, L"%.2fg");
+	CompareUpdateValue(cshouldery, m_cshouldersY, m_hWnd, IDC_SKELETON_CSHOULDER_Y_READING, L"%.2fg");
+	CompareUpdateValue(cshoulderz, m_cshouldersZ, m_hWnd, IDC_SKELETON_CSHOULDER_Z_READING, L"%.2fg");
+	CompareUpdateValue(spinex, m_spineX, m_hWnd, IDC_SKELETON_SPINE_X_READING, L"%.2fg");
+	CompareUpdateValue(spiney, m_spineY, m_hWnd, IDC_SKELETON_SPINE_Y_READING, L"%.2fg");
+	CompareUpdateValue(spinez, m_spineZ, m_hWnd, IDC_SKELETON_SPINE_Z_READING, L"%.2fg");
+	CompareUpdateValue(chipx, m_chipX, m_hWnd, IDC_SKELETON_CHIP_X_READING, L"%.2fg");
+	CompareUpdateValue(chipy, m_chipY, m_hWnd, IDC_SKELETON_CHIP_Y_READING, L"%.2fg");
+	CompareUpdateValue(chipz, m_chipZ, m_hWnd, IDC_SKELETON_CHIP_Z_READING, L"%.2fg");
+	CompareUpdateValue(rhandx, m_rhandX, m_hWnd, IDC_SKELETON_RHAND_X_READING, L"%.2fg");
+	CompareUpdateValue(rhandy, m_rhandY, m_hWnd, IDC_SKELETON_RHAND_Y_READING, L"%.2fg");
+	CompareUpdateValue(rhandz, m_rhandZ, m_hWnd, IDC_SKELETON_RHAND_Z_READING, L"%.2fg");
+	CompareUpdateValue(rwristx, m_rwristX, m_hWnd, IDC_SKELETON_RWRIST_X_READING, L"%.2fg");
+	CompareUpdateValue(rwristy, m_rwristY, m_hWnd, IDC_SKELETON_RWRIST_Y_READING, L"%.2fg");
+	CompareUpdateValue(rwristz, m_rwristZ, m_hWnd, IDC_SKELETON_RWRIST_Z_READING, L"%.2fg");
+	CompareUpdateValue(relbowx, m_relbowX, m_hWnd, IDC_SKELETON_RELBOW_X_READING, L"%.2fg");
+	CompareUpdateValue(relbowy, m_relbowY, m_hWnd, IDC_SKELETON_RELBOW_Y_READING, L"%.2fg");
+	CompareUpdateValue(relbowz, m_relbowZ, m_hWnd, IDC_SKELETON_RELBOW_Z_READING, L"%.2fg");
+	CompareUpdateValue(rshoulderx, m_rshoulderX, m_hWnd, IDC_SKELETON_RSHOULDER_X_READING, L"%.2fg");
+	CompareUpdateValue(rshouldery, m_rshoulderY, m_hWnd, IDC_SKELETON_RSHOULDER_Y_READING, L"%.2fg");
+	CompareUpdateValue(rshoulderz, m_rshoulderZ, m_hWnd, IDC_SKELETON_RSHOULDER_Z_READING, L"%.2fg");
+	CompareUpdateValue(rhipx, m_rhipX, m_hWnd, IDC_SKELETON_RHIP_X_READING, L"%.2fg");
+	CompareUpdateValue(rhipy, m_rhipY, m_hWnd, IDC_SKELETON_RHIP_Y_READING, L"%.2fg");
+	CompareUpdateValue(rhipz, m_rhipZ, m_hWnd, IDC_SKELETON_RHIP_Z_READING, L"%.2fg");
+	CompareUpdateValue(rkneex, m_rkneeX, m_hWnd, IDC_SKELETON_RKNEE_X_READING, L"%.2fg");
+	CompareUpdateValue(rkneey, m_rkneeY, m_hWnd, IDC_SKELETON_RKNEE_Y_READING, L"%.2fg");
+	CompareUpdateValue(rkneez, m_rkneeZ, m_hWnd, IDC_SKELETON_RKNEE_Z_READING, L"%.2fg");
+	CompareUpdateValue(ranklex, m_rankleX, m_hWnd, IDC_SKELETON_RANKLE_X_READING, L"%.2fg");
+	CompareUpdateValue(rankley, m_rankleY, m_hWnd, IDC_SKELETON_RANKLE_Y_READING, L"%.2fg");
+	CompareUpdateValue(ranklez, m_rankleZ, m_hWnd, IDC_SKELETON_RANKLE_Z_READING, L"%.2fg");
+	CompareUpdateValue(rfootx, m_rfootX, m_hWnd, IDC_SKELETON_RFOOT_X_READING, L"%.2fg");
+	CompareUpdateValue(rfooty, m_rfootY, m_hWnd, IDC_SKELETON_RFOOT_Y_READING, L"%.2fg");
+	CompareUpdateValue(rfootz, m_rfootZ, m_hWnd, IDC_SKELETON_RFOOT_Z_READING, L"%.2fg");
+	CompareUpdateValue(lhandx, m_lhandX, m_hWnd, IDC_SKELETON_LHAND_X_READING, L"%.2fg");
+	CompareUpdateValue(lhandy, m_lhandY, m_hWnd, IDC_SKELETON_LHAND_Y_READING, L"%.2fg");
+	CompareUpdateValue(lhandz, m_lhandZ, m_hWnd, IDC_SKELETON_LHAND_Z_READING, L"%.2fg");
+	CompareUpdateValue(lwristx, m_lwristX, m_hWnd, IDC_SKELETON_LWRIST_X_READING, L"%.2fg");
+	CompareUpdateValue(lwristy, m_lwristY, m_hWnd, IDC_SKELETON_LWRIST_Y_READING, L"%.2fg");
+	CompareUpdateValue(lwristz, m_lwristZ, m_hWnd, IDC_SKELETON_LWRIST_Z_READING, L"%.2fg");
+	CompareUpdateValue(lelbowx, m_lelbowX, m_hWnd, IDC_SKELETON_LELBOW_X_READING, L"%.2fg");
+	CompareUpdateValue(lelbowy, m_lelbowY, m_hWnd, IDC_SKELETON_LELBOW_Y_READING, L"%.2fg");
+	CompareUpdateValue(lelbowz, m_lelbowZ, m_hWnd, IDC_SKELETON_LELBOW_Z_READING, L"%.2fg");
+	CompareUpdateValue(lshoulderx, m_lshoulderX, m_hWnd, IDC_SKELETON_LSHOULDER_X_READING, L"%.2fg");
+	CompareUpdateValue(lshouldery, m_lshoulderY, m_hWnd, IDC_SKELETON_LSHOULDER_Y_READING, L"%.2fg");
+	CompareUpdateValue(lshoulderz, m_lshoulderZ, m_hWnd, IDC_SKELETON_LSHOULDER_Z_READING, L"%.2fg");
+	CompareUpdateValue(lhipx, m_lhipX, m_hWnd, IDC_SKELETON_LHIP_X_READING, L"%.2fg");
+	CompareUpdateValue(lhipy, m_lhipY, m_hWnd, IDC_SKELETON_LHIP_Y_READING, L"%.2fg");
+	CompareUpdateValue(lhipz, m_lhipZ, m_hWnd, IDC_SKELETON_LHIP_Z_READING, L"%.2fg");
+	CompareUpdateValue(lkneex, m_lkneeX, m_hWnd, IDC_SKELETON_LKNEE_X_READING, L"%.2fg");
+	CompareUpdateValue(lkneey, m_lkneeY, m_hWnd, IDC_SKELETON_LKNEE_Y_READING, L"%.2fg");
+	CompareUpdateValue(lkneez, m_lkneeZ, m_hWnd, IDC_SKELETON_LKNEE_Z_READING, L"%.2fg");
+	CompareUpdateValue(lanklex, m_lankleX, m_hWnd, IDC_SKELETON_LANKLE_X_READING, L"%.2fg");
+	CompareUpdateValue(lankley, m_lankleY, m_hWnd, IDC_SKELETON_LANKLE_Y_READING, L"%.2fg");
+	CompareUpdateValue(lanklez, m_lankleZ, m_hWnd, IDC_SKELETON_LANKLE_Z_READING, L"%.2fg");
+	CompareUpdateValue(lfootx, m_lfootX, m_hWnd, IDC_SKELETON_LFOOT_X_READING, L"%.2fg");
+	CompareUpdateValue(lfooty, m_lfootY, m_hWnd, IDC_SKELETON_LFOOT_Y_READING, L"%.2fg");
+	CompareUpdateValue(lfootz, m_lfootZ, m_hWnd, IDC_SKELETON_LFOOT_Z_READING, L"%.2fg");
+}
---
+++ starship/NuiSkeletonPointsViewer.h
@@ -0,0 +1,156 @@
+//------------------------------------------------------------------------------
+// <copyright file="NuiAccelerometerViewer.h" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+#pragma once
+
+#include "Utility.h"
+#include "NuiViewer.h"
+
+class KinectWindow;
+
+class NuiSkeletonPointsViewer : public NuiViewer
+{
+public:
+	/**
+	* Constructor
+	* @param  pParent  The pointer to parent window
+	*/
+	NuiSkeletonPointsViewer(const NuiViewer* pParent);
+
+	/**
+	 * Destructor
+	 */
+	~NuiSkeletonPointsViewer();
+
+public:
+	/**
+	 * Set skeleton 3D position readings to display
+	 * @param  headx
+	 * ...
+	 * @param lfoot
+	 */
+	void SetSkeletonPointsReadings(FLOAT headx,      FLOAT heady,      FLOAT headz,
+		                           FLOAT cshoulderx, FLOAT cshouldery, FLOAT cshoulderz,
+								   FLOAT spinex,     FLOAT spiney,     FLOAT spinez, 
+								   FLOAT chipx,      FLOAT chipy,      FLOAT chipz, 
+								   FLOAT rhandx,     FLOAT rhandy,     FLOAT rhandz, 
+								   FLOAT rwristx,    FLOAT rwristy,    FLOAT rwristz,
+								   FLOAT relbowx,    FLOAT relbowy,    FLOAT relbowz,
+								   FLOAT rshoulderx, FLOAT rshouldery, FLOAT rshoulderz,
+								   FLOAT rhipx,      FLOAT rhipy,      FLOAT rhipz,
+								   FLOAT rkneex,     FLOAT rkneey,     FLOAT rkneez, 
+								   FLOAT ranklex,    FLOAT rankley,    FLOAT ranklez,
+								   FLOAT rfootx,     FLOAT rfooty,     FLOAT rfootz,
+								   FLOAT lhandx,     FLOAT lhandy,     FLOAT lhandz,
+								   FLOAT lwristx,    FLOAT lwristy,    FLOAT lwristz,
+								   FLOAT lelbowx,    FLOAT lelbowy,    FLOAT lelbowz,
+								   FLOAT lshoulderx, FLOAT lshouldery, FLOAT lshoulderz,
+								   FLOAT lhipx,      FLOAT lhipy,      FLOAT lhipz,
+								   FLOAT lkneex,     FLOAT lkneey,     FLOAT lkneez,
+								   FLOAT lanklex,    FLOAT lankley,    FLOAT lanklez,
+								   FLOAT lfootx,     FLOAT lfooty,     FLOAT lfootz);
+
+private:
+	/**
+	 * Returns the id of the dialog
+	 * @return Id of dialog
+	 */
+	virtual UINT GetDlgId();
+
+
+	/**
+	 * Process message or send it to coreponding handler
+	 * @param   hWnd    The handle to the window which receives the message
+	 * @param   uMsg    Message identifier
+	 * @param   wParam  Additional message information
+	 * @param   lParam  Additional message information
+	 * @return  Result of message process. Depends on message type
+	 */
+	virtual LRESULT DialogProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+private:
+	// 3D position coordinates of skeletal points
+	// TODO: should probably put this in an array
+	FLOAT m_headX;
+	FLOAT m_headY;
+	FLOAT m_headZ;
+
+	FLOAT m_cshouldersX;
+	FLOAT m_cshouldersY;
+	FLOAT m_cshouldersZ;
+
+	FLOAT m_spineX;
+	FLOAT m_spineY;
+	FLOAT m_spineZ;
+
+	FLOAT m_chipX;
+	FLOAT m_chipY;
+	FLOAT m_chipZ;
+
+	FLOAT m_rhandX;
+	FLOAT m_rhandY;
+	FLOAT m_rhandZ;
+
+	FLOAT m_rwristX;
+	FLOAT m_rwristY;
+	FLOAT m_rwristZ;
+
+	FLOAT m_relbowX;
+	FLOAT m_relbowY;
+	FLOAT m_relbowZ;
+
+	FLOAT m_rshoulderX;
+	FLOAT m_rshoulderY;
+	FLOAT m_rshoulderZ;
+
+	FLOAT m_rhipX;
+	FLOAT m_rhipY;
+	FLOAT m_rhipZ;
+
+	FLOAT m_rkneeX;
+	FLOAT m_rkneeY;
+	FLOAT m_rkneeZ;
+
+	FLOAT m_rankleX;
+	FLOAT m_rankleY;
+	FLOAT m_rankleZ;
+
+	FLOAT m_rfootX;
+	FLOAT m_rfootY;
+	FLOAT m_rfootZ;
+
+	FLOAT m_lhandX;
+	FLOAT m_lhandY;
+	FLOAT m_lhandZ;
+
+	FLOAT m_lwristX;
+	FLOAT m_lwristY;
+	FLOAT m_lwristZ;
+
+	FLOAT m_lelbowX;
+	FLOAT m_lelbowY;
+	FLOAT m_lelbowZ;
+
+	FLOAT m_lshoulderX;
+	FLOAT m_lshoulderY;
+	FLOAT m_lshoulderZ;
+
+	FLOAT m_lhipX;
+	FLOAT m_lhipY;
+	FLOAT m_lhipZ;
+
+	FLOAT m_lkneeX;
+	FLOAT m_lkneeY;
+	FLOAT m_lkneeZ;
+
+	FLOAT m_lankleX;
+	FLOAT m_lankleY;
+	FLOAT m_lankleZ;
+
+	FLOAT m_lfootX;
+	FLOAT m_lfootY;
+	FLOAT m_lfootZ;
+};
