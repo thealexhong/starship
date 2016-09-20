d11485be2102ec4d8f65a0fd21c1d1635df964c1
Yuma <yuma.tsuboi@gmail.com>
2016-09-20 17:50:07 -0400

    yuma's new voice model based on vocal features

--- Voice Analysis/VoiceFeatureExtraction/classify.bat
+++ Voice Analysis/VoiceFeatureExtraction/classify.bat
@@ -1,2 +1,2 @@
-java -classpath "weka.jar" weka.classifiers.functions.MultilayerPerceptron -T rt_valence.arff -l valence_MultilayerPerceptron_779.model -p 0
-java -classpath "weka.jar" weka.classifiers.functions.MultilayerPerceptron -T rt_arousal.arff -l arousal_MultilayerPerceptron_825.model -p 0
+java -classpath "weka.jar" weka.classifiers.functions.MultilayerPerceptron -T rt_valence.arff -l valence_MultilayerPerceptron_794.model -p 0
+java -classpath "weka.jar" weka.classifiers.functions.MultilayerPerceptron -T rt_arousal.arff -l arousal_MultilayerPerceptron_844.model -p 0
